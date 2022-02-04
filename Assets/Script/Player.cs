5ffc74a0312dbdde554a0c6513e7d148fd1abca5
inugasha <53877578+inugasha@users.noreply.github.com>
2022-02-04 12:35:50 +0100

    Refacto

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,45 +3,45 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    public float JumpHeight;
-    public Vector3 Jump;
-    public float JumpForce = 2.0f;
-    public bool IsGrounded;
-     
+    public float JumpForce = 10.0f;
+
+    private bool _isGrounded;
+
     private Rigidbody _rb;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        Jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
-     
-    void OnCollisionStay()
+
+    private void OnCollisionEnter(Collision collision)
     {
-        IsGrounded = true;
-    }
-     
-     void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded)
-        {    
-            _rb.AddForce(Jump * JumpForce, ForceMode.Impulse);
-            IsGrounded = false;
+        if (collision.gameObject.tag == "Floor")
+        {
+            _isGrounded = true;
         }
     }
-    
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            _isGrounded = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            _isGrounded = false;
+        }
+    }
+
     public void Die()
     {
         GameManager.Instance.GameOver();
-        
     }
-    /*public void Jump()
-    {
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            
-            myRb.AddForce(Vector2.up * jumpHeight, ForceMode.Impulse);
-        }
-    }
-   */
 }
