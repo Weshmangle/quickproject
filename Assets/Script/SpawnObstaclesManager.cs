1e08d3a537b376ad70775703cd0d1d2a8b97c0d6
inugasha <53877578+inugasha@users.noreply.github.com>
2022-02-04 11:47:49 +0100

    Update SpawnObstaclesManager.cs

--- Assets/Script/SpawnObstaclesManager.cs
+++ Assets/Script/SpawnObstaclesManager.cs
@@ -3,19 +3,33 @@ using System.Collections;
 
 public class SpawnObstaclesManager : MonoBehaviour
 {
-    //liste de prefab (obstacle, bonus, )
-    //position des object
-    //position de la map
-    //position limite de la map (en gros a partir de quand on suprime le vieux terrain et genere le nouveau)
-    //vitesse de defilement
-    //instantiation des terrain
-    //supression des terrain
-    
     public static SpawnObstaclesManager Instance;
+    public float Delay
+    {
+        get { return _delay; }
+        private set { _delay = value; }
+    }
+    public bool SpawnObstacle
+    {
+        get
+        {
+            return _spawnObstacle;
+        }
+        private set
+        {
+            _spawnObstacle = value;
+            if (!_spawnObstacle)
+            {
+                StopAllCoroutines();
+            }
+        }
+    }
 
-    private float _delay = 1;
+    [SerializeField] private float _delay = 1;
 
     [SerializeField] private GameObject[] _prefabs;
+
+    private bool _spawnObstacle = true;
 
     private void Awake()
     {
@@ -35,26 +49,21 @@ public class SpawnObstaclesManager : MonoBehaviour
 
     private IEnumerator SpawnAfterTime()
     {
-        yield return new WaitForSeconds(_delay);
+        while (_spawnObstacle)
+        {
+            yield return new WaitForSeconds(_delay);
 
-        var instance = Instantiate(GetRandomPrefab(), transform.position, Quaternion.identity, transform);
-        var scale = Random.Range(0.5f, 1);
-        instance.transform.localScale = new Vector3(scale, scale, scale);
-
-        StartCoroutine(SpawnAfterTime());
+            var instance = Instantiate(GetRandomPrefab(), transform.position, Quaternion.identity, transform);
+            var scale = Random.Range(.5f, 1f);
+            instance.transform.localScale = new Vector3(scale, scale, scale);
+        }
     }
     private GameObject GetRandomPrefab()
     {
         return _prefabs[Random.Range(0, _prefabs.Length)];
     }
 
-    public float Delay
-    {
-        get { return _delay; }
-        private set { _delay = value; }
-    }
-
-    public void StartSpawn()
+    private void StartSpawn()
     {
         StartCoroutine(SpawnAfterTime());
     }
