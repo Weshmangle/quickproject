98bf3df35cf4dc5f335e501d3ae3b3dbe6eccd2e
inugasha <53877578+inugasha@users.noreply.github.com>
2022-02-09 14:14:33 +0100

    Dino animation start

--- Unity/Assets/Script/Player.cs
+++ Unity/Assets/Script/Player.cs
@@ -5,16 +5,32 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject[] _meshesAnimationsDown;
     [SerializeField] private GameObject[] _meshesAnimationNormal;
+    [SerializeField] private float _animationDuration;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask obstacleLayer;
     [SerializeField] private AudioClip _jumpSound;
     [SerializeField] private float stopCrouchTime, gameTime;
 
-    public bool _isGrounded, _isCrouched;
+    private bool _isGrounded;
+    private bool _isCrouched;
     private float jumpHeight = 1f;
     private float gravity = -100f;
     private float stepAnimation = 0;
     private CharacterController _cc;
+    private bool _playAnimation = true;
+
+    public bool IsCrouched
+    {
+        get { return _isCrouched; }
+        set
+        {
+            _isCrouched = value;
+            StopAllCoroutines();
+            _playAnimation = true;
+            StartCoroutine(Animation());
+        }
+    }
+
 
     public Vector3 positionGravity;
 
@@ -31,7 +47,7 @@ public class Player : MonoBehaviour
     public void OnJump()
     {
         Debug.Log("OnJump");
-        if(GameManager.Instance.IsGameOver)
+        if (GameManager.Instance.IsGameOver)
         {
             GameManager.Instance.GameStart();
         }
@@ -83,16 +99,58 @@ public class Player : MonoBehaviour
             var scale = transform.localScale;
             scale.y = 1f;
             transform.localScale = scale;
-            stepAnimation = (stepAnimation + .05f) % _meshesAnimationNormal.Length;
+            //stepAnimation = (stepAnimation + .05f) % _meshesAnimationNormal.Length;
 
-            foreach (var mesh in _meshesAnimationNormal)
-            {
-                mesh.SetActive(false);
-            }
+            //foreach (var mesh in _meshesAnimationNormal)
+            //{
+            //    mesh.SetActive(false);
+            //}
 
-            _meshesAnimationNormal[Mathf.FloorToInt(stepAnimation)].SetActive(true);
+            //_meshesAnimationNormal[Mathf.FloorToInt(stepAnimation)].SetActive(true);
         }
     }
+
+    public void StartAnimation()
+    {
+        StartCoroutine(Animation());
+    }
+
+    private IEnumerator Animation()
+    {
+        while (_playAnimation)
+        {
+            if (_isCrouched)
+            {
+                foreach (var item in _meshesAnimationNormal)
+                {
+                    item.SetActive(false);
+                }
+
+                _meshesAnimationsDown[0].SetActive(true);
+                _meshesAnimationsDown[1].SetActive(false);
+                yield return new WaitForSeconds(_animationDuration);
+                _meshesAnimationsDown[0].SetActive(false);
+                _meshesAnimationsDown[1].SetActive(true);
+                yield return new WaitForSeconds(_animationDuration);
+            }
+            else
+            {
+                foreach (var item in _meshesAnimationsDown)
+                {
+                    item.SetActive(false);
+                }
+
+                _meshesAnimationNormal[0].SetActive(true);
+                _meshesAnimationNormal[1].SetActive(false);
+                yield return new WaitForSeconds(_animationDuration);
+                _meshesAnimationNormal[0].SetActive(false);
+                _meshesAnimationNormal[1].SetActive(true);
+                yield return new WaitForSeconds(_animationDuration);
+            }
+        }
+
+    }
+
 
     public void Jump()
     {
@@ -109,6 +167,7 @@ public class Player : MonoBehaviour
     }
     public void Die()
     {
+        StopAllCoroutines();
         GameManager.Instance.GameOver();
     }
 
