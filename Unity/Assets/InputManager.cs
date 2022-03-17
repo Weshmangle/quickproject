3936e28416f82d8fe4a24da381e1c85d16ea2908
Weshmangle <96111319+Weshmangle@users.noreply.github.com>
2022-03-17 15:26:06 +0100

    Add click button UI

--- Unity/Assets/InputManager.cs
+++ Unity/Assets/InputManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
     private TouchControls touchControls;
+    public int stateSpeaker = 0;
 
     private void Awake() {
         touchControls = new TouchControls();
@@ -35,12 +37,19 @@ public class InputManager : MonoBehaviour
         else
         {
             var position = touchControls.Touch.TouchPosition.ReadValue<Vector2>();
-
-            //GameManager.Instance.buttonAbout.transform.
             
-            if(position.x < Screen.width / 2)
+            if(isClickedButtonSpeak(position))
             {
-                Player.Instance.Jump();
+                Debug.Log("speak");
+                stateSpeaker = (stateSpeaker + 1) % 3;
+            }
+            else if(isClickedButtonAbout(position))
+            {
+                Debug.Log("about");
+            }
+            else if(position.x < Screen.width / 2)
+            {
+                Player.Instance.Jump(); 
             }
             else
             {
@@ -48,11 +57,33 @@ public class InputManager : MonoBehaviour
             }
         }
     }
+    
     private void EndTouch(InputAction.CallbackContext context)
     {
         if(Player.Instance.IsCrouched)
         {
             Player.Instance.IsCrouched = false; 
         }
+    }
+
+    private bool isClickedButton(Button button, Vector2 position)
+    {
+        var rect = (button.transform as RectTransform).rect;
+        var pos = button.transform.position;
+
+        return position.x > pos.x + rect.x &&
+               position.x < pos.x + rect.width &&
+               position.y > pos.y + rect.y &&
+               position.y < pos.y + rect.height;
+    }
+
+    private bool isClickedButtonSpeak(Vector2 position)
+    {
+        return isClickedButton(GameManager.Instance.buttonSpeaker, position);
+    }
+
+    private bool isClickedButtonAbout(Vector2 position)
+    {
+        return isClickedButton(GameManager.Instance.buttonAbout, position);
     }
 }
